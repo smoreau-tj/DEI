1e1abd4b01270344a39ac6edc1692718834dfa0a
Serge M <serge@tommyjohn.com>
2021-04-13 21:22:46 -0400

    global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,8 +32,11 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  styleResources: {
+    scss: ['./assets/scss/*.scss'],
+  },
 }
