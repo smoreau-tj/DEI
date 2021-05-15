ddcf81f6e462535196e274f47db6abfc2d9c7aa4
Serge M <serge@tommyjohnwear.com>
2021-05-15 11:15:38 -0400

    WIP: preparing aapi page

---
+++ pages/aapi.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class="header">
+      <h1>AAPI</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+import data from '@/assets/data/council'
+export default {
+  data() {
+    return {
+      content: data.aapi,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+  width: 100%;
+  min-width: 100vw;
+  min-height: 100vh;
+
+  h1 {
+    margin: 0 auto;
+    padding: 0 1rem;
+  }
+}
+</style>
